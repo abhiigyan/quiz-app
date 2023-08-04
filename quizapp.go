ad176ecc17a8fb6083bb90177cecd5632228b45c
Abhigyan Shrivastava <118006762+abhiigyan@users.noreply.github.com>
2023-08-04 19:57:59 +0530

    Update quizapp.go

--- quizapp.go
+++ quizapp.go
@@ -1,10 +1,21 @@
 package main
 
 import(
-	""
+		"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
 )
 func ProbPull(fileName string)([]probs,error ){
 //reads from quiz.csv
+		if fObj, err := os.Open(fileName); err == nil {
+		csvR := csv.NewReader(fObj)
+		if cLines, err := csvR.ReadAll(); err == nil {
+			return parseProb(cLines), nil
+		}
+	}
 //open file
 //create reader
 //read it
@@ -13,12 +24,20 @@ func ProbPull(fileName string)([]probs,error ){
 
 func main(){
 	//input name of file
+	fName := flag.String("f", "quiz.csv", "path of csv file")
 	//set the duration of timer
+	timer := flag.Int("t", 20, "timer for the quiz")
 	//pull the problems from the files
-	//error handling
-	//var to count correct answers
-	//loop for problems and printing them
-	//result
+	flag.Parse()
+	probs, err := ProbPull()
+
+	if err != nil {
+		exit(fmt.Sprintf("something went wrong:%s", err.Error()))
+	}
+	correct := 0
+
+	tObj := time.NewTimer(time.Duration(*timer) * time.Second)
+	ansC := make(chan string)
 }
 
 func parseProb(lines [][]string)[]probs{
